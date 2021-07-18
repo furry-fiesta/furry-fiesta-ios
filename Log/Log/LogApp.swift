79c0039c24b98efe303d66f9b6a245c8dd666449
Andras Kadar <kadarandrass@gmail.com>
2021-07-18 10:34:04 +0200

    Create Log Xcode Project
    Create a new Xcode project, named Log as a SwiftUI app with tests on Xcode 12.5.1
    
    Bundle id: com.furryfiesta.log

---
+++ Log/Log/LogApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogApp.swift
+//  Log
+//
+//  Created by Andras Kadar on 18.07.21.
+//
+
+import SwiftUI
+
+@main
+struct LogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
